0e482707ceb23082c73fd4e4d392b196cccaaced
Jonas Nicklas <jonas.nicklas@gmail.com>
2012-11-23 16:08:29 +0100

    Extract repeat into separate parser class

--- lib/expgen/parser.rb
+++ lib/expgen/parser.rb
@@ -1,28 +1,35 @@
 module Expgen
   NON_LITERALS = "[]\^$.|?*+()".split("").map { |l| "\\" + l }.join
 
-  class Parser < Parslet::Parser
-    rule(:lparen)     { str('(') }
-    rule(:rparen)     { str(')') }
-    rule(:lbracket)   { str('[') }
-    rule(:rbracket)   { str(']') }
+  class Repeat < Parslet::Parser
     rule(:lcurly)     { str('{') }
     rule(:rcurly)     { str('}') }
-    rule(:pipe)       { str('|') }
-    rule(:plus)       { str('+') }
     rule(:multiply)   { str('*') }
-    rule(:dash)       { str('-') }
+    rule(:plus)       { str('+') }
     rule(:comma)      { str(',') }
     rule(:backslash)  { str('\\') }
     rule(:questionmark) { str('?') }
 
     rule(:integer) { match('[0-9]').repeat(1).as(:int) }
-    rule(:literal) { match["^#{NON_LITERALS}"].as(:letter) >> repeat.maybe  }
 
     rule(:repeat_amount) { lcurly >> integer.as(:repeat) >> (comma >> integer.as(:max).maybe).maybe >> rcurly }
     rule(:repeat) { plus.as(:repeat) | multiply.as(:repeat) | questionmark.as(:optional) | repeat_amount }
 
-    rule(:group) { lparen >> expression.as(:elements) >> rparen >> repeat.maybe }
+    root(:repeat)
+  end
+
+  class Parser < Parslet::Parser
+    rule(:lparen)     { str('(') }
+    rule(:rparen)     { str(')') }
+    rule(:lbracket)   { str('[') }
+    rule(:rbracket)   { str(']') }
+    rule(:pipe)       { str('|') }
+    rule(:dash)       { str('-') }
+    rule(:backslash)  { str('\\') }
+
+    rule(:literal) { match["^#{NON_LITERALS}"].as(:letter) >> Repeat.new.maybe  }
+
+    rule(:group) { lparen >> expression.as(:elements) >> rparen >> Repeat.new.maybe }
 
     # character classes
     rule(:alpha) { match["a-z"] }
@@ -30,15 +37,15 @@ module Expgen
     rule(:char) { match["^#{NON_LITERALS}"].as(:letter) }
     rule(:range) { (alpha.as(:from) >> dash >> alpha.as(:to)) | (number.as(:from) >> dash >> number.as(:to)) }
     rule(:char_class) do
-      lbracket >> match["\\^"].maybe.as(:negative) >> ( char_class_shorthand.as(:char_class_shorthand) | escape_char | range.as(:char_class_range) | char.as(:char_class_literal)).repeat.as(:groups) >> rbracket >> repeat.maybe
+      lbracket >> match["\\^"].maybe.as(:negative) >> ( char_class_shorthand.as(:char_class_shorthand) | escape_char | range.as(:char_class_range) | char.as(:char_class_literal)).repeat.as(:groups) >> rbracket >> Repeat.new.maybe
     end
-    rule(:char_class_shorthand) { backslash >> match["wWdDhHsS"].as(:letter) >> repeat.maybe }
+    rule(:char_class_shorthand) { backslash >> match["wWdDhHsS"].as(:letter) >> Repeat.new.maybe }
 
-    rule(:code_point_octal) { backslash >> match["0-7"].repeat(3,3).as(:code) >> repeat.maybe }
-    rule(:code_point_hex) { backslash >> str("x") >> match["0-9a-fA-F"].repeat(2,2).as(:code) >> repeat.maybe }
-    rule(:code_point_unicode) { backslash >> str("u") >> match["0-9a-fA-F"].repeat(4,4).as(:code) >> repeat.maybe }
-    rule(:escape_char_control) { backslash >> match["nsrtvfae"].as(:letter) >> repeat.maybe }
-    rule(:escape_char_literal) { backslash >> any.as(:letter) >> repeat.maybe }
+    rule(:code_point_octal) { backslash >> match["0-7"].repeat(3,3).as(:code) >> Repeat.new.maybe }
+    rule(:code_point_hex) { backslash >> str("x") >> match["0-9a-fA-F"].repeat(2,2).as(:code) >> Repeat.new.maybe }
+    rule(:code_point_unicode) { backslash >> str("u") >> match["0-9a-fA-F"].repeat(4,4).as(:code) >> Repeat.new.maybe }
+    rule(:escape_char_control) { backslash >> match["nsrtvfae"].as(:letter) >> Repeat.new.maybe }
+    rule(:escape_char_literal) { backslash >> any.as(:letter) >> Repeat.new.maybe }
 
     rule(:escape_char) { escape_char_control.as(:escape_char_control) | code_point_octal.as(:code_point_octal) | code_point_hex.as(:code_point_hex) | code_point_unicode.as(:code_point_unicode) | escape_char_literal.as(:escape_char_literal) }
 
